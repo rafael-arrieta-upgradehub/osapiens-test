73dd491505260d68030de8223f8e33495eb0e22a
rarrieta <rarrieta@rarrietas-MacBook-Pro.local>
2024-11-25 03:08:59 +0100

    feat: 24 nov

--- components/__tests__/WeatherDetails.test.tsx
+++ components/__tests__/WeatherDetails.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import WeatherDetails from '@/components/WeatherDetails';
 describe('TemperatureDisplay', () => {
-    it('renderslñmmñlmription', () => {
+    it('renders correctly', () => {
         const tree = renderer.create(<WeatherDetails temperature={25.1234} wind={10.5678} />).toJSON();
 
         expect(tree).toMatchSnapshot();
